e04200e040e86b21f98b6d886fa7c822854501e3
Sarath Kumar <nagarajsarath@gmail.com>
2019-09-16 19:46:14 -0400

    Updates

---
+++ precache-manifest.06bfe6f880f2ee8d21a3f624f0e07b53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7c0343e54e1d11028077fceecdd8c7",
+    "url": "/sarathkumar6.github.io/Monsters_React_App/index.html"
+  },
+  {
+    "revision": "fa3034164b4ea527777f",
+    "url": "/sarathkumar6.github.io/Monsters_React_App/static/css/main.3deb299b.chunk.css"
+  },
+  {
+    "revision": "4bfab55fe7cb3b5330b6",
+    "url": "/sarathkumar6.github.io/Monsters_React_App/static/js/2.89644ae7.chunk.js"
+  },
+  {
+    "revision": "fa3034164b4ea527777f",
+    "url": "/sarathkumar6.github.io/Monsters_React_App/static/js/main.4296c0b4.chunk.js"
+  },
+  {
+    "revision": "8d6f876ec7d0e900d985",
+    "url": "/sarathkumar6.github.io/Monsters_React_App/static/js/runtime~main.a33a80da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sarathkumar6.github.io/Monsters_React_App/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sarathkumar6.github.io/Monsters_React_App/precache-manifest.06bfe6f880f2ee8d21a3f624f0e07b53.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sarathkumar6.github.io/Monsters_React_App/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
